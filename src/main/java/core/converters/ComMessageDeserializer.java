b742968d5af98477a835f9a7cdd594b148c7470b
gayoung <orz4120@gmail.com>
2023-11-25 12:50:23 +0900

    fix deseri-

--- src/main/java/core/converters/ComMessageDeserializer.java
+++ src/main/java/core/converters/ComMessageDeserializer.java
@@ -3,21 +3,21 @@ package core.converters;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import core.dto.MtcExgRequest;
+import core.dto.MtcNcrPayRequest;
+import core.dto.MtcNcrSdaMainMasRequest;
 import org.apache.commons.lang3.SerializationException;
 import org.apache.kafka.common.serialization.Deserializer;
 
-
-public class ExgMessageDeserializer implements Deserializer<MtcExgRequest> {
+public class ComMessageDeserializer implements Deserializer<MtcNcrSdaMainMasRequest> {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
-    public MtcExgRequest deserialize(String s, byte[] bytes) {
+    public MtcNcrSdaMainMasRequest deserialize(String s, byte[] bytes) {
         try {
-            return objectMapper.readValue(new String(bytes), MtcExgRequest.class);
+            return objectMapper.readValue(new String(bytes), MtcNcrSdaMainMasRequest.class);
         } catch (JsonProcessingException e) {
             System.out.println(e.getOriginalMessage());
             throw new SerializationException(e);
         }
     }
 }
-
